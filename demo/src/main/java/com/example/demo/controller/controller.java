8b3d251dd3cd01f452d798dd1e63ffd52a5768de
Lovekush Nagda <nagdalovekush220@gmail.com>
2025-05-02 23:25:41 +0530

    Login Feature

---
+++ demo/src/main/java/com/example/demo/controller/controller.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.*;
+@RestController
+public class controller{
+	@RequestMapping("/FetchData")
+	public String getName() {
+		return "Lovekush Nagda";
+	}
+	@RequestMapping("/GetId")
+	public int getId() {
+		return 39;
+	}
+    @RequestMapping("/Details")
+    public String getDetails() {
+    	return "Name = " +getName()+ " and Id =" +getId();
+    }
+    @RequestMapping("/Info")
+    public String getInfo(@RequestParam("un") String un,@RequestParam("id") int id) {
+    	return "Name =" +un "and Id ="+id;
+    }
+}
+
+
