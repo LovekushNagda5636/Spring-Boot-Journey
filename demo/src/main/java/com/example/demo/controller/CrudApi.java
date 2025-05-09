032cd765719ab585164d9d927bc03dd8835f5edc
Lovekush Nagda <nagdalovekush220@gmail.com>
2025-05-09 22:01:26 +0530

    Day-7

--- demo/src/main/java/com/example/demo/controller/CrudApi.java
+++ demo/src/main/java/com/example/demo/controller/CrudApi.java
@@ -4,8 +4,6 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,9 +22,9 @@ public class CrudApi{
 	@Autowired
 	private CrudImpl impl;
 	@PostMapping("/user")
-	public ResponseEntity<User> createUser(@RequestBody User user){
+	public User createUser(@RequestBody User user){
 		user=impl.createUser(user);
-		return new ResponseEntity<User>(user, HttpStatus.OK);
+		return user;
 	}
 	@GetMapping("/user")
 	public List<User> allUser(){
